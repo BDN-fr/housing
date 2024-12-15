275abd0f9d39568da0376739a3fb34db945eb9af
BDN_fr <lucasjoly2008@gmail.com>
2024-12-16 00:05:23 +0100

    Init

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BDN_fr'
+description 'An open-source housing script'
+version '0.0.1'
+repository 'https://github.com/BDN-fr/housing'
+
+client_scripts {
+    'client/**.lua',
+    'config/client/**.lua'
+}
+
+server_scripts {
+    'server/**.lua',
+    'config/server/**.lua'
+}
+
+shared_scripts {
+    'config/shared/**.lua'
+}
